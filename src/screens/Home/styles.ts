09a3235c5c50780ba2674703512379547a50a314
Yevhenii <eucheremisin@gmail.com>
2021-09-17 11:27:21 +0300

    YC: *design-+env

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,37 +1,45 @@
-import { StyleSheet } from "react-native";
+import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-    container: {
-        height: '100%',
-    },
-    header: {
-        padding: 10,
-        backgroundColor: '#eef'
-    },
-    headerText: {
-        fontSize: 22
-    },
-    image: {
-        width: 440,
-        height: 440,
-    },
-    imageItem: {
-        display: 'flex',
-        flexDirection: 'row',
-        borderRadius: 10,
-        overflow: 'hidden',
-        marginBottom: 20,
-        backgroundColor: '#eee'
-    },
-    list: {
-        padding: 10,
-    },
-    imageItemInfo: {
-        padding: 10,
-    },
-    imageInfoAuthor: {
-        color: '#118'
-    }
-})
+  container: {
+    height: '100%',
+  },
+  header: {
+    padding: 10,
+    backgroundColor: '#eef',
+  },
+  headerText: {
+    fontSize: 22,
+  },
+  image: {
+    height: 200,
+    flex: 1,
+  },
+  imageItem: {
+    display: 'flex',
+    flexDirection: 'row',
+    borderRadius: 10,
+    overflow: 'hidden',
+    marginBottom: 20,
+    backgroundColor: '#eee',
+  },
+  list: {
+    padding: 10,
+  },
+  imageItemInfo: {
+    padding: 10,
+    backgroundColor: 'rgba(0,0,0,0.4)',
+    height: 90,
+    marginTop: 110,
+  },
+  imageInfoAuthor: {
+    color: '#fff',
+  },
+  imageInfoTitle: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+});
 
-export default styles
+export default styles;
